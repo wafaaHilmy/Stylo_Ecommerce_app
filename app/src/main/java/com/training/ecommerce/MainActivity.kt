8f36e4aed89861fc95649666a564c71f08ab3509
wafaaHilmy <wafaa.hilmy@gmail.com>
2024-03-03 20:07:02 +0200

    add login activity-viewModel-fragment

--- app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -2,12 +2,29 @@ package com.training.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.training.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var  binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-         installSplashScreen()
-        setContentView(R.layout.activity_main)
+
+       binding=ActivityMainBinding.inflate(layoutInflater)
+
+        installSplashScreen()
+        setContentView(binding.root)
+
+
+    binding.textView.setOnClickListener(View.OnClickListener {
+          Log.d(MainActivity::class.simpleName, "test button pressed")
+
+          throw RuntimeException("Test Crash") // Force a crash
+      })
+
     }
 }
---
+++ app/src/main/java/com/training/ecommerce/ui/login/LoginViewModel.kt
@@ -0,0 +1,14 @@
+package com.training.ecommerce.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel:ViewModel() {
+
+
+
+
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
