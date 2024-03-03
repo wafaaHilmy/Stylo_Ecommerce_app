2b0d5f0241ed6f823646ec219b17f1d6fd94dd61
wafaaHilmy <wafaa.hilmy@gmail.com>
2024-03-03 20:46:04 +0200

    add navigation component

--- app/src/main/java/com/training/ecommerce/ui/login/LoginActivity.kt
+++ app/src/main/java/com/training/ecommerce/ui/login/LoginActivity.kt
@@ -2,11 +2,31 @@ package com.training.ecommerce.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.training.ecommerce.R
+import com.training.ecommerce.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityLoginBinding
+   private lateinit var loginViewModel: LoginViewModel
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loginViewModel=ViewModelProvider(this).get(LoginViewModel::class.java)
+        // Obtain reference to the NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.login_nav_host_fragment) as NavHostFragment
+        // Get the NavController
+        navController = navHostFragment.navController
+        // Set up the ActionBar with the Navigation UI
+
+
     }
 }
